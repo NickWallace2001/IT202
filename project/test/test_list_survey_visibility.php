83bb1fcdf8730660a0c11f49b205af55d6e0a237
NickWallace2001 <ncw6@njit.edu>
2020-11-10 20:43:48 +0000

    searching accounting for visibility

---
+++ project/test/test_list_survey_visibility.php
@@ -0,0 +1,65 @@
+<?php require_once(__DIR__ . "/../partials/nav.php");?>
+<?php
+if (!has_role("Admin")){
+    flash("You don't have permission to access this page");
+    die(header("Location: ../login.php"));
+}
+?>
+<?php
+$query = "";
+$results = [];
+$visibility = 0;
+if (isset($_POST["query"])){
+    $query = $_POST["query"];
+}
+if (isset($_POST["search"]) && !empty($query)){
+    $db = getDB();
+    $stmt = $db->prepare("SELECT visibility from Survey where Survey.visibility = :visibility");
+    $stmt->execute([":visibility" => $visibility]);
+    if (getVisibility($visibility) == 0 || getVisibility($visibility) == 1){
+        $stmt = $db->prepare("SELECT id, title, description, user_id, Users.id FROM Survey where user_id = Users.id AND title like :q LIMIT 10");
+        $r = $stmt->execute([":q" => "%$query%"]);
+    }
+    elseif (getVisibility($visibility) == 2) {
+        $stmt = $db->prepare("SELECT id, title, description, visibility from Survey WHERE title like :q LIMIT 10");
+        $r = $stmt->execute([":q" => "%$query%"]);
+    }
+    if ($r) {
+        $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    }
+    else{
+        flash("There was a problem fetching the results");
+    }
+}
+?>
+<form method="POST">
+    <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
+    <input type="submit" value="Search" name="search"/>
+</form>
+<div class="results">
+    <?php if (count($results) > 0): ?>
+    <div class="list-group">
+        <?php foreach ($results as $r): ?>
+        <div class="list-group-item">
+            <div>
+                <div>Title:</div>
+                <div><?php safer_echo($r["title"]); ?></div>
+            </div>
+            <div>
+                <div>Description:</div>
+                <div><?php safer_echo($r["description"]); ?></div>
+            </div>
+            <div>
+                <div>Visibility:</div>
+                <div><?php getVisibility($r["visibility"]); ?></div>
+            </div>
+            <div>
+                <a type="button" href="test_edit_survey.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
+                <a type="button" href="test_view_survey.php?id=<?php safer_echo($r['id']); ?>">View</a>
+            </div>
+            <?php endforeach; ?>
+        </div>
+        <?php else: ?>
+            <p>No results</p>
+        <?php endif; ?>
+    </div>

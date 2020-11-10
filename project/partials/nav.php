95e235de1e2e03d6d884700abf17a7313fbc5f96
NickWallace2001 <ncw6@njit.edu>
2020-11-10 17:29:54 +0000

    finishing cleanup

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -7,20 +7,20 @@ require_once(__DIR__ . "/../lib/helpers.php");
 
 <nav>
 <ul class="nav">
-    <li><a href="<?php getURL("home.php"); ?>">Home</a></li>
+    <li><a href="<?php echo getURL("home.php"); ?>">Home</a></li>
     <?php if (!is_logged_in()): ?>
         <li><a href="<?php echo getURL("login.php"); ?>">Login</a></li>
         <li><a href="<?php echo getURL("register.php"); ?>">Register</a></li>
     <?php endif; ?>
     <?php if (has_role("Admin")): ?>
-	    <li><a href="<?php getURL("test/test_create_survey.php"); ?>">Create Survey</a></li>
-	    <li><a href="<?php getURL("test/test_list_survey.php"); ?>">View Surveys</a></li>
-        <li><a href="<?php getURL("test/test_create_question.php"); ?>">Create Question</a></li>
-        <li><a href="<?php getURL("test/test_list_questions.php"); ?>">View Questions</a></li>
+	    <li><a href="<?php echo getURL("test/test_create_survey.php"); ?>">Create Survey</a></li>
+	    <li><a href="<?php echo getURL("test/test_list_survey.php"); ?>">View Surveys</a></li>
+        <li><a href="<?php echo getURL("test/test_create_question.php"); ?>">Create Question</a></li>
+        <li><a href="<?php echo getURL("test/test_list_questions.php"); ?>">View Questions</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
-        <li><a href="<?php getURL("profile.php"); ?>">Profile</a></li>
-        <li><a href="<?php getURL("logout.php"); ?>">Logout</a></li>
+        <li><a href="<?php echo getURL("profile.php"); ?>">Profile</a></li>
+        <li><a href="<?php echo getURL("logout.php"); ?>">Logout</a></li>
     <?php endif; ?>
 </ul>
 </nav>
